054f5453620c38afbae076a828b6292781a3f091
matiaslog <flaviomatiasnf@gmail.com>
2024-10-16 13:03:39 +0000

    java

---
+++ aulas/aula_03/main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args)
+    {
+        Scanner banana = new Scanner(System.in);
+
+        int a = banana.nextInt();
+
+        int b = banana.nextInt();
+
+        // int soma = a + b;
+
+        System.out.println("soma:" + a + b);        
+        
+        banana.close();
+
+
+
+
+    }
+}
