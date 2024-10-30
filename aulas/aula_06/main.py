72e30bf7944c8eb96e895b2b70177cd00a2a27de
FLAVIO DA SILVA MATIAS <20241014040040@ifrn.local>
2024-10-30 10:23:22 -0300

    npc

--- aulas/aula_06/main.py
+++ aulas/aula_06/main.py
@@ -5,20 +5,55 @@ class Monster:
         self.speed = speed
         self.sprite = sprite
         self.rect = rect
+
+    def __str__(self):
+        return "a monster"
     def move(self):
         print("andou")
     def atack(self):
         print("atacando")
         
+class NPC:
+    def __init__(self, life, speed, sprite, rect):
+        self.life = life
+        self.speed = speed
+        self.sprite = sprite
+        self.rect = rect
+        self.clt = "ajudante de pedreiro"
+
+    def comer():
+        pass
+
+    def troca(self, moeda):
+        pass
     
-class Lobo(Monster):
+    def talking():
+        pass
+    
+    def move():
+        pass
+        
+    
+class Wolf(Monster):
     def __init__(self):
         super().__init__(life=100, atk=20, speed=15, sprite="lobo_sprite", rect=(30, 50))
+
+    def __str__(self): 
+        return f"({super().__str__()})wolf : {self.life}"
 
 class Goblin(Monster):
     def __init__(self):
         super().__init__(life=50, atk=10, speed=20, sprite="goblin_sprite", rect=(20, 40))
+        
+    def __str__(self): 
+        return f"goblin : {self.life}"
 
-# Exemplo de instâncias
-lobo = Lobo()
-print(f"Lobo -> Vida: {lobo.life}, Ataque: {lobo.atk}, Velocidade: {lobo.speed}")
+
+class Vilage(NPC):
+    def __init__(self):
+        super().__init__(life= 100, speed= 2, sprite= ("lalalla"), rect = (20, 40))
+
+
+
+lobo = Wolf()
+print(lobo)
